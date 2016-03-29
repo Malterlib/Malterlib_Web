1af0ad5f258c3582dc3520d1678a23d99ea77102
Erik Olofsson <erik@olofsson.info>
2016-03-29 18:27:02 +0200

    [Malterlib/Web] Use new random id funcionality

--- Source/Malterlib_Web_DDP_Client.cpp
+++ Source/Malterlib_Web_DDP_Client.cpp
@@ -3,6 +3,7 @@
 #include <Mib/Network/Sockets/SSL>
 #include <Mib/Web/HTTP/Request>
 #include <Mib/Cryptography/Hashes/SHA>
+#include <Mib/Cryptography/RandomID>
 #include <Mib/Concurrency/Actor/Timer>
 #include <Mib/Concurrency/ConcurrencyDefines>
 
@@ -1101,44 +1102,14 @@ namespace NMib
 				m_Observations.f_Remove(_pColletionObservations);
 		}		
 
-		namespace
-		{
-			ch8 g_UnmistakableChars[] = "23456789ABCDEFGHJKLMNPQRSTWXYZabcdefghijkmnopqrstuvwxyz";
-		}
-
 		NStr::CStr CDDPClient::fs_RandomID()
 		{
-			const mint c_nCharsInRandomID = 17;
-			const mint c_nChars = sizeof(g_UnmistakableChars) / sizeof(g_UnmistakableChars[0]) - 1;
-			const mint c_BufferSize = ((c_nCharsInRandomID + 3) / 4) * 4;
-			uint8 RandomData[c_BufferSize];
-
-			for (mint i = 0; i < sizeof(RandomData) / 4; ++i)
-				*((uint32 *)(RandomData + i*4)) = NMisc::fg_GetRandomUnsigned();
-
-			NStr::CStr Return;
-
-			for (mint i = 0; i < c_nCharsInRandomID; ++i)
-				Return.f_AddChar(g_UnmistakableChars[RandomData[i] % c_nChars]);
-
-			return Return;
+			return NCryptography::fg_RandomID();
 		}
 
 		NStr::CStr CDDPClient::fs_HighEntropyRandomID()
 		{
-			const mint c_nCharsInRandomID = 17;
-			const mint c_nChars = sizeof(g_UnmistakableChars) / sizeof(g_UnmistakableChars[0]) - 1;
-			const mint c_BufferSize = ((c_nCharsInRandomID + 3) / 4) * 4;
-			uint8 RandomData[c_BufferSize];
-			
-			NSys::fg_Security_GenerateHighEntropyData(RandomData, c_BufferSize);
-
-			NStr::CStr Return;
-
-			for (mint i = 0; i < c_nCharsInRandomID; ++i)
-				Return.f_AddChar(g_UnmistakableChars[RandomData[i] % c_nChars]);
-
-			return Return;
+			return NCryptography::fg_HighEntropyRandomID();
 		}
 	}
 }
