5c6fde916f685a07aa51a3967ebc3be9bc942c71
Erik Olofsson <erik.olofsson@hansoft.com>
2016-08-07 21:41:34 +0000

    [Malterlib/Web] Add support for listen flags
    Copied from Perforce
     Change: 69838

--- Test/Test_Malterlib_Web_WebSocket.cpp
+++ Test/Test_Malterlib_Web_WebSocket.cpp
@@ -138,6 +138,7 @@ public:
 				(
 					&CWebSocketServerActor::f_StartListenAddress
 					, fg_CreateVector(ListenAddress)
+					, NMib::NNet::ENetFlag_None
 					, NMib::NConcurrency::fg_ConcurrentActor()
 					, [pState](CWebSocketNewServerConnection &&_ConnectionInfo)
 					{
