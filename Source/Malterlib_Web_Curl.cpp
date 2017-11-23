50938dcfc1cd518be5ac1f4d28666e55cd96c330
Erik Olofsson <erik@olofsson.info>
2017-11-23 18:11:03 +0100

    [Malterlib/Web] Report exceptions in Curl actor

--- Source/Malterlib_Web_Curl.cpp
+++ Source/Malterlib_Web_Curl.cpp
@@ -67,117 +67,122 @@ namespace NMib::NWeb
 			, NStr::CStr const &_Data
 		)
 	{
-		CStr Data(_Data);
-		
-		*g_CurlInit;
-
-		::CURL *pCurl = curl_easy_init();
-		
-		if (!pCurl)
-			DMibError("libcurl was not initialised");
-		
-		auto CleanupCurl = g_OnScopeExit > [&]
+		return NConcurrency::TCContinuation<CCurlActor::CResult>::fs_RunProtected() > [&]
 			{
-				curl_easy_cleanup(pCurl);
-			}
-		;
 
-		CStr CurlErrorBuffer;
-		CurlErrorBuffer.f_CreateWritableBuffer(CURL_ERROR_SIZE, true);
+				CStr Data(_Data);
 
-		auto fCheckResult =
-			[&](CURLcode _Result) -> void
-			{
-				if (_Result != CURLE_OK)
-					DMibError(fg_Format("libcurl operation on {} failed: {}", _URL, CurlErrorBuffer));
-			}
-		;
-		
-		curl_easy_setopt(pCurl, CURLOPT_ERRORBUFFER, CurlErrorBuffer.f_GetStr());
-		
-		
-		curl_slist *pHeaders = NULL;
-		auto CleanupHeaders = g_OnScopeExit > [&]
-			{
-				curl_slist_free_all(pHeaders);
-			}
-		;
+				*g_CurlInit;
 
-		pHeaders = curl_slist_append(pHeaders, "Accept: application/json");
-		pHeaders = curl_slist_append(pHeaders, "Content-Type: application/json");
-		pHeaders = curl_slist_append(pHeaders, "Expect:");
+				::CURL *pCurl = curl_easy_init();
 
-		if (_Method == EMethod_POST)
-		{
-			fCheckResult(curl_easy_setopt(pCurl, CURLOPT_POST, 1L));
-			fCheckResult(curl_easy_setopt(pCurl, CURLOPT_POSTFIELDS, Data.f_GetStr()));
-			fCheckResult(curl_easy_setopt(pCurl, CURLOPT_POSTFIELDSIZE, Data.f_GetLen()));
-		}
-		else if (_Method == EMethod_PUT)
-		{
-			fCheckResult(curl_easy_setopt(pCurl, CURLOPT_UPLOAD, 1L));
-			fCheckResult(curl_easy_setopt(pCurl, CURLOPT_READDATA, &Data));
-			fCheckResult(curl_easy_setopt(pCurl, CURLOPT_INFILESIZE, Data.f_GetLen()));
-			
-			auto fReadCallback =
-				[](char *_pBuffer, size_t _Size, size_t _nItems, void *_pData) -> size_t
-				{
-					size_t Bytes = _Size * _nItems;
+				if (!pCurl)
+					DMibError("libcurl was not initialised");
 
-					if (Bytes > 0)
+				auto CleanupCurl = g_OnScopeExit > [&]
 					{
-						CStr &DataSource = *static_cast<CStr*>(_pData);
-						NMem::fg_MemCopy(_pBuffer, DataSource.f_GetStr(), Bytes);
-						DataSource = DataSource.f_Extract(Bytes);
+						curl_easy_cleanup(pCurl);
 					}
-					
-					return Bytes;
+				;
+
+				CStr CurlErrorBuffer;
+				CurlErrorBuffer.f_CreateWritableBuffer(CURL_ERROR_SIZE, true);
+
+				auto fCheckResult =
+					[&](CURLcode _Result) -> void
+					{
+						if (_Result != CURLE_OK)
+							DMibError(fg_Format("libcurl operation on {} failed: {}", _URL, CurlErrorBuffer));
+					}
+				;
+
+				curl_easy_setopt(pCurl, CURLOPT_ERRORBUFFER, CurlErrorBuffer.f_GetStr());
+
+
+				curl_slist *pHeaders = NULL;
+				auto CleanupHeaders = g_OnScopeExit > [&]
+					{
+						curl_slist_free_all(pHeaders);
+					}
+				;
+
+				pHeaders = curl_slist_append(pHeaders, "Accept: application/json");
+				pHeaders = curl_slist_append(pHeaders, "Content-Type: application/json");
+				pHeaders = curl_slist_append(pHeaders, "Expect:");
+
+				if (_Method == EMethod_POST)
+				{
+					fCheckResult(curl_easy_setopt(pCurl, CURLOPT_POST, 1L));
+					fCheckResult(curl_easy_setopt(pCurl, CURLOPT_POSTFIELDS, Data.f_GetStr()));
+					fCheckResult(curl_easy_setopt(pCurl, CURLOPT_POSTFIELDSIZE, Data.f_GetLen()));
 				}
-			;
-			
-			fCheckResult(curl_easy_setopt(pCurl, CURLOPT_READFUNCTION, (curl_read_callback)fReadCallback));
-		}
-		else if (_Method == EMethod_DELETE)
-		{
-			fCheckResult(curl_easy_setopt(pCurl, CURLOPT_CUSTOMREQUEST, "DELETE"));
-		}
-		
-		fCheckResult(curl_easy_setopt(pCurl, CURLOPT_URL, _URL.f_GetStr()));
-		
-		for (auto &Header : _Headers)
-		{
-			CStr HeaderStr(fg_Format("{}: {}", _Headers.fs_GetKey(Header), Header));
-			pHeaders = curl_slist_append(pHeaders, HeaderStr.f_GetStr());
-		}
-		fCheckResult(curl_easy_setopt(pCurl, CURLOPT_HTTPHEADER, pHeaders));
+				else if (_Method == EMethod_PUT)
+				{
+					fCheckResult(curl_easy_setopt(pCurl, CURLOPT_UPLOAD, 1L));
+					fCheckResult(curl_easy_setopt(pCurl, CURLOPT_READDATA, &Data));
+					fCheckResult(curl_easy_setopt(pCurl, CURLOPT_INFILESIZE, Data.f_GetLen()));
 
-		CState State;
+					auto fReadCallback =
+						[](char *_pBuffer, size_t _Size, size_t _nItems, void *_pData) -> size_t
+						{
+							size_t Bytes = _Size * _nItems;
 
-		fCheckResult(curl_easy_setopt(pCurl, CURLOPT_HEADERDATA, &State));
-		auto fWriteHeaderCallback =
-			[](char *_pBuffer, size_t _Size, size_t _nItems, void *_pData) -> size_t
-			{
-				CState &State = *static_cast<CState *>(_pData);
-				State.m_Headers.f_Insert((uint8 const *)_pBuffer, _Size * _nItems);
-				return _Size * _nItems;
+							if (Bytes > 0)
+							{
+								CStr &DataSource = *static_cast<CStr*>(_pData);
+								NMem::fg_MemCopy(_pBuffer, DataSource.f_GetStr(), Bytes);
+								DataSource = DataSource.f_Extract(Bytes);
+							}
+
+							return Bytes;
+						}
+					;
+
+					fCheckResult(curl_easy_setopt(pCurl, CURLOPT_READFUNCTION, (curl_read_callback)fReadCallback));
+				}
+				else if (_Method == EMethod_DELETE)
+				{
+					fCheckResult(curl_easy_setopt(pCurl, CURLOPT_CUSTOMREQUEST, "DELETE"));
+				}
+
+				fCheckResult(curl_easy_setopt(pCurl, CURLOPT_URL, _URL.f_GetStr()));
+
+				for (auto &Header : _Headers)
+				{
+					CStr HeaderStr(fg_Format("{}: {}", _Headers.fs_GetKey(Header), Header));
+					pHeaders = curl_slist_append(pHeaders, HeaderStr.f_GetStr());
+				}
+				fCheckResult(curl_easy_setopt(pCurl, CURLOPT_HTTPHEADER, pHeaders));
+
+				CState State;
+
+				fCheckResult(curl_easy_setopt(pCurl, CURLOPT_HEADERDATA, &State));
+				auto fWriteHeaderCallback =
+					[](char *_pBuffer, size_t _Size, size_t _nItems, void *_pData) -> size_t
+					{
+						CState &State = *static_cast<CState *>(_pData);
+						State.m_Headers.f_Insert((uint8 const *)_pBuffer, _Size * _nItems);
+						return _Size * _nItems;
+					}
+				;
+				fCheckResult(curl_easy_setopt(pCurl, CURLOPT_HEADERFUNCTION, (curl_write_callback)fWriteHeaderCallback));
+
+				fCheckResult(curl_easy_setopt(pCurl, CURLOPT_WRITEDATA, &State));
+				auto fWriteBodyCallback =
+					[](char *_pBuffer, size_t _Size, size_t _nItems, void *_pData) -> size_t
+					{
+						CState &State = *static_cast<CState *>(_pData);
+						State.m_Body.f_Insert((uint8 const *)_pBuffer, _Size * _nItems);
+						return _Size * _nItems;
+					}
+				;
+				fCheckResult(curl_easy_setopt(pCurl, CURLOPT_WRITEFUNCTION, (curl_write_callback)fWriteBodyCallback));
+
+				fCheckResult(curl_easy_perform(pCurl));
+
+				CCurlActor::CResult Result(State);
+				return Result;
 			}
 		;
-		fCheckResult(curl_easy_setopt(pCurl, CURLOPT_HEADERFUNCTION, (curl_write_callback)fWriteHeaderCallback));
-		
-		fCheckResult(curl_easy_setopt(pCurl, CURLOPT_WRITEDATA, &State));
-		auto fWriteBodyCallback =
-			[](char *_pBuffer, size_t _Size, size_t _nItems, void *_pData) -> size_t 
-			{
-				CState &State = *static_cast<CState *>(_pData);
-				State.m_Body.f_Insert((uint8 const *)_pBuffer, _Size * _nItems);
-				return _Size * _nItems;
-			}
-		;
-		fCheckResult(curl_easy_setopt(pCurl, CURLOPT_WRITEFUNCTION, (curl_write_callback)fWriteBodyCallback));
-
-		fCheckResult(curl_easy_perform(pCurl));
-	
-		CCurlActor::CResult Result(State);
-		return fg_Explicit(Result);
 	}
 }
