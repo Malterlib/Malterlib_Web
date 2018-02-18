e6e2a40d75acd5e98ad330170e5d256190b0fb84
Erik Olofsson <erik@olofsson.info>
2018-02-18 10:34:10 +0100

    [Malterlib/Web] Refactor websocket listen

--- Source/Malterlib_Web_WebSocketServerActor.cpp
+++ Source/Malterlib_Web_WebSocketServerActor.cpp
@@ -133,69 +133,16 @@ namespace NMib
 				, NNet::FVirtualSocketFactory &&_SocketFactory
 			)
 		{
-			NNet::FVirtualSocketFactory SocketFactory = fg_Move(_SocketFactory);
-			if (!SocketFactory)
-				SocketFactory = NNet::CSocket_TCP::fs_GetFactory();
-			
-			NConcurrency::TCContinuation<NConcurrency::CActorSubscription> Ret;
-			try
+			NContainer::TCVector<NNet::CNetAddress> AddressesToListenTo;
+			for (mint i = 0; i < _nListen; ++i)
 			{
-				if (!mp_pInternal->m_ListenSockets.f_IsEmpty())
-					DMibError("Socket server is already listening");
-			
-				mp_pInternal->f_Clear();
-				auto NewReference = mp_pInternal->m_OnNewConnection.f_Register(_Actor, fg_Move(_fNewConnection));
-				auto FailedReference = mp_pInternal->m_OnFailedConnection.f_Register(_Actor, fg_Move(_fFailedConnection));
-				auto Reference = fg_CombinedCallbackReference(fg_Move(NewReference), fg_Move(FailedReference));
-				
-				mp_pInternal->m_ListenSockets.f_SetLen(_nListen);
-
-				for (mint i = 0; i < _nListen; ++i)
-				{
-					NNet::CNetAddressTCPv4 AnyAddress;
-					AnyAddress.m_Port = _StartListen + i;
-					NNet::CNetAddress Address;
-					Address.f_Set(AnyAddress);
-
-					NConcurrency::TCActor<CListenActor> &ListenActor = mp_pInternal->m_ListenSockets[i];
-					ListenActor = NConcurrency::fg_ConstructActor<CListenActor>(fg_ThisActor(this), mp_pInternal->m_MaxMessageSize, mp_pInternal->m_FragmentationSize, mp_pInternal->m_Timeout);
-					
-					NConcurrency::TCWeakActor<CListenActor> WeakListenActor = ListenActor;
-					
-					NPtr::TCUniquePointer<NNet::ICSocket> pListenSocket = SocketFactory();
-					NException::CDisableExceptionTraceScope DisableExceptionTrace;
-					pListenSocket->f_Listen
-						(
-							Address
-							, [WeakListenActor](NNet::ENetTCPState _StateAdded)
-							{
-								
-								auto ListenActor = WeakListenActor.f_Lock();
-								if (ListenActor)
-								{
-									ListenActor(&CListenActor::f_StateAdded, _StateAdded)
-										> NConcurrency::fg_DiscardResult()
-									;
-								}
-							}
-							, _ListenFlags
-						)
-					;
-					
-					ListenActor(&CListenActor::f_SetSocket, fg_Move(pListenSocket))
-						> NConcurrency::fg_DiscardResult()
-					;
-				}			
-				
-				Ret.f_SetResult(fg_Move(Reference));
-				
+				NNet::CNetAddressTCPv4 AnyAddress;
+				AnyAddress.m_Port = _StartListen + i;
+				NNet::CNetAddress Address;
+				Address.f_Set(AnyAddress);
+				AddressesToListenTo.f_Insert(fg_Move(Address));
 			}
-			catch (NException::CException const &)
-			{
-				mp_pInternal->f_Clear();
-				Ret.f_SetCurrentException();
-			}		
-			return Ret;
+			return f_StartListenAddress(fg_Move(AddressesToListenTo), _ListenFlags, _Actor, fg_Move(_fNewConnection), fg_Move(_fFailedConnection), fg_Move(_SocketFactory));
 		}
 
 		NConcurrency::TCContinuation<void> CWebSocketServerActor::fp_Destroy()
