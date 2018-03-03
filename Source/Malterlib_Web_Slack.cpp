1ae16b2ce297a231d6bbee6a9a5a2e198bbeda53
Erik Olofsson <erik@olofsson.info>
2018-03-03 08:36:00 +0100

    [Malterlib/Web] Add Slack actor with support for incoming webhooks

---
+++ Source/Malterlib_Web_Slack.cpp
@@ -0,0 +1,182 @@
+// Copyright © 2018 Nonna Holding AB
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#include "Malterlib_Web_Slack.h"
+#include "Mib/Encoding/JSON"
+#include "Mib/Encoding/JSONShortcuts"
+#include "Mib/Web/Curl"
+
+namespace NMib::NWeb
+{
+	using namespace NEncoding;
+	using namespace NStorage;
+	using namespace NContainer;
+	using namespace NStr;
+
+	struct CSlackActor::CInternal
+	{
+		CInternal(NConcurrency::TCActor<CCurlActor> const &_CurlActor)
+			: m_CurlActor{_CurlActor}
+		{
+		}
+
+		NConcurrency::TCActor<CCurlActor> m_CurlActor;
+	};
+
+	CSlackActor::CSlackActor(NConcurrency::TCActor<CCurlActor> const &_CurlActor)
+		: mp_pInternal{fg_Construct(_CurlActor)}
+	{
+	}
+
+	CSlackActor::~CSlackActor() = default;
+
+
+	CStr CSlackActor::CMessage::fs_EscapeString(CStr const &_String)
+	{
+		CUStr UnicodeString = _String;
+
+		CUStr OutString;
+
+		for (auto pParse = UnicodeString.f_GetStr(); *pParse; ++pParse)
+		{
+			switch (*pParse)
+			{
+			case '`':
+			case '<':
+			case '>':
+			case '*':
+				OutString.f_AddChar(0x0C);
+				OutString.f_AddChar(*pParse);
+				OutString.f_AddChar(0x0C);
+				break;
+			default:
+				OutString.f_AddChar(*pParse);
+				break;
+			}
+		}
+
+		return OutString;
+	}
+
+	NConcurrency::TCContinuation<void> CSlackActor::f_SendMessage(NHTTP::CURL const &_IncomingWebhook, CMessage const &_Message)
+	{
+		CEJSON SlackMessage(EJSONType_Object);
+
+		if (_Message.m_Text)
+			SlackMessage["text"] = *_Message.m_Text;
+		if (_Message.m_Channel)
+			SlackMessage["channel"] = *_Message.m_Channel;
+		if (_Message.m_UserName)
+			SlackMessage["username"] = *_Message.m_UserName;
+		if (_Message.m_IconEmoji)
+			SlackMessage["icon_emoji"] = *_Message.m_IconEmoji;
+		if (_Message.m_IconURL)
+			SlackMessage["icon_url"] = *_Message.m_IconURL->f_Encode();
+
+		if (_Message.m_bMarkdown)
+			SlackMessage["mrkdwn"] = *_Message.m_bMarkdown;
+		if (_Message.m_bLinkNames)
+			SlackMessage["link_names"] = *_Message.m_bLinkNames;
+		if (_Message.m_bReplyBroadcast)
+			SlackMessage["reply_broadcast"] = *_Message.m_bReplyBroadcast;
+		if (_Message.m_bUnfurlLinks)
+			SlackMessage["unfurl_links"] = *_Message.m_bUnfurlLinks;
+		if (_Message.m_bUnfurlMedia)
+			SlackMessage["unfurl_media"] = *_Message.m_bUnfurlMedia;
+		if (_Message.m_bFullParse)
+			SlackMessage["unfurl_media"] = *_Message.m_bFullParse ? "full" : "none";
+
+		if (_Message.m_ThreadTimestamp)
+			SlackMessage["thread_ts"] = NTime::CTimeConvert(*_Message.m_ThreadTimestamp).f_UnixSecondsFraction();
+
+		for (auto &Attachment : _Message.m_Attachments)
+		{
+			auto &OutputAttachment = SlackMessage["attachments"].f_Array().f_Insert() =
+				{
+					"fallback"_= Attachment.m_Fallback
+					, "title"_= Attachment.m_Title
+					, "text"_= Attachment.m_Text
+				}
+			;
+
+			if (Attachment.m_bPretextMarkdown && *Attachment.m_bPretextMarkdown)
+				OutputAttachment["mrkdwn_in"].f_Array().f_Insert("pretext");
+			if (Attachment.m_bTextMarkdown && *Attachment.m_bTextMarkdown)
+				OutputAttachment["mrkdwn_in"].f_Array().f_Insert("text");
+			if (Attachment.m_bFieldsMarkdown && *Attachment.m_bFieldsMarkdown)
+				OutputAttachment["mrkdwn_in"].f_Array().f_Insert("fields");
+
+			if (Attachment.m_PreText)
+				OutputAttachment["pretext"] = *Attachment.m_PreText;
+			if (Attachment.m_TitleLink)
+				OutputAttachment["title_link"] = *Attachment.m_TitleLink;
+
+			if (Attachment.m_Color)
+			{
+				if (Attachment.m_Color->f_GetTypeID() == 0)
+				{
+					auto ColorEnum = Attachment.m_Color->f_Get<0>();
+					switch (ColorEnum)
+					{
+					case EPredefinedColor_Good: OutputAttachment["color"] = "good"; break;
+					case EPredefinedColor_Warning: OutputAttachment["color"] = "warning"; break;
+					case EPredefinedColor_Danger: OutputAttachment["color"] = "danger"; break;
+					}
+				}
+				else
+				{
+					auto &Color = Attachment.m_Color->f_Get<1>();
+					OutputAttachment["color"] = "#{nfh,sj2,sf0}{nfh,sj2,sf0}{nfh,sj2,sf0}"_f << Color.m_Red << Color.m_Green << Color.m_Blue;
+				}
+			}
+
+     		if (Attachment.m_AuthorName)
+				OutputAttachment["author_name"] = *Attachment.m_AuthorName;
+			if (Attachment.m_AuthorLink)
+				OutputAttachment["author_link"] = Attachment.m_AuthorLink->f_Encode();
+			if (Attachment.m_AuthorIcon)
+				OutputAttachment["author_icon"] = Attachment.m_AuthorIcon->f_Encode();
+
+			if (Attachment.m_ImageURL)
+				OutputAttachment["image_url"] = Attachment.m_ImageURL->f_Encode();
+			if (Attachment.m_ThumbURL)
+				OutputAttachment["thumb_url"] = Attachment.m_ThumbURL->f_Encode();
+
+     		if (Attachment.m_Footer)
+				OutputAttachment["footer"] = *Attachment.m_Footer;
+			if (Attachment.m_FooterIconURL)
+				OutputAttachment["thumb_url"] = Attachment.m_FooterIconURL->f_Encode();
+     		if (Attachment.m_FooterTimestamp)
+				OutputAttachment["ts"] = NTime::CTimeConvert(*Attachment.m_FooterTimestamp).f_UnixSeconds();
+
+			for (auto &Field : Attachment.m_Fields)
+			{
+				auto &OutputField = OutputAttachment["fields"].f_Array().f_Insert() =
+					{
+						"title"_= Field.m_Title
+						, "value"_= Field.m_Value
+					}
+				;
+
+				if (Field.m_bShort)
+					OutputField["short"] = *Field.m_bShort;
+			}
+		}
+
+		NConcurrency::TCContinuation<void> Continuation;
+		auto &Internal = *mp_pInternal;
+		Internal.m_CurlActor(&CCurlActor::f_Request,CCurlActor::EMethod_POST, _IncomingWebhook.f_Encode(), TCMap<CStr, CStr>{}, SlackMessage.f_ToString())
+			> Continuation / [Continuation](CCurlActor::CResult &&_Result)
+			{
+				if (_Result.m_StatusCode != 200)
+				{
+					Continuation.f_SetException(DMibErrorInstance("Slack request failed with status {}: {}"_f << _Result.m_StatusCode << _Result.m_Body));
+					return;
+				}
+				Continuation.f_SetResult();
+			}
+		;
+
+		return Continuation;
+	}
+}
---
+++ Source/Malterlib_Web_Slack.h
@@ -0,0 +1,107 @@
+// Copyright © 2018 Nonna Holding AB
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#pragma once
+
+#include <Mib/Core/Core>
+#include <Mib/Concurrency/ConcurrencyManager>
+#include <Mib/Encoding/EJSON>
+#include <Mib/Storage/Optional>
+#include <Mib/Web/HTTP/URL>
+
+namespace NMib::NWeb
+{
+	struct CCurlActor;
+
+	struct CSlackActor : public NConcurrency::CActor
+	{
+		enum EPredefinedColor
+		{
+			EPredefinedColor_Good
+			, EPredefinedColor_Warning
+			, EPredefinedColor_Danger
+		};
+
+		struct CRgbColor
+		{
+			uint8 m_Red = 0;
+			uint8 m_Green = 0;
+			uint8 m_Blue = 0;
+		};
+
+		using CColor = NContainer::TCVariant<EPredefinedColor, CRgbColor>;
+
+		struct CField
+		{
+			NStr::CStr m_Title;
+			NStr::CStr m_Value;
+			NStorage::TCOptional<bool> m_bShort;
+		};
+
+		struct CAttachment
+		{
+			NStr::CStr m_Fallback;
+
+			NStorage::TCOptional<NStr::CStr> m_PreText;
+			NStorage::TCOptional<bool> m_bPretextMarkdown;
+
+			NStr::CStr m_Title;
+			NStorage::TCOptional<NStr::CStr> m_TitleLink;
+
+			NStr::CStr m_Text;
+			NStorage::TCOptional<bool> m_bTextMarkdown;
+
+			NStorage::TCOptional<CColor> m_Color;
+
+			NStorage::TCOptional<NStr::CStr> m_AuthorName;
+			NStorage::TCOptional<NHTTP::CURL> m_AuthorLink;
+			NStorage::TCOptional<NHTTP::CURL> m_AuthorIcon;
+
+			NContainer::TCVector<CField> m_Fields;
+			NStorage::TCOptional<bool> m_bFieldsMarkdown;
+
+			NStorage::TCOptional<NHTTP::CURL> m_ImageURL;
+			NStorage::TCOptional<NHTTP::CURL> m_ThumbURL;
+
+			NStorage::TCOptional<NStr::CStr> m_Footer;
+			NStorage::TCOptional<NHTTP::CURL> m_FooterIconURL;
+			NStorage::TCOptional<NTime::CTime> m_FooterTimestamp;
+		};
+
+		struct CMessage
+		{
+			NStorage::TCOptional<NStr::CStr> m_Text;
+			NStorage::TCOptional<NStr::CStr> m_Channel;
+			NStorage::TCOptional<NStr::CStr> m_UserName;
+			NStorage::TCOptional<NStr::CStr> m_IconEmoji;
+			NStorage::TCOptional<NHTTP::CURL> m_IconURL;
+
+			NStorage::TCOptional<bool> m_bMarkdown;
+			NStorage::TCOptional<bool> m_bLinkNames;
+			NStorage::TCOptional<bool> m_bReplyBroadcast;
+			NStorage::TCOptional<bool> m_bUnfurlLinks;
+			NStorage::TCOptional<bool> m_bUnfurlMedia;
+			NStorage::TCOptional<bool> m_bFullParse;
+
+			NStorage::TCOptional<NTime::CTime> m_ThreadTimestamp;
+
+			NContainer::TCVector<CAttachment> m_Attachments;
+
+			static NStr::CStr fs_EscapeString(NStr::CStr const &_String); // Escapes string so formatting is not applied
+		};
+
+		NConcurrency::TCContinuation<void> f_SendMessage(NHTTP::CURL const &_IncomingWebhook, CMessage const &_Message);
+
+		CSlackActor(NConcurrency::TCActor<CCurlActor> const &_CurlActor);
+		~CSlackActor();
+
+	private:
+		struct CInternal;
+
+		NPtr::TCUniquePointer<CInternal> mp_pInternal;
+	};
+}
+
+#ifndef DMibPNoShortCuts
+using namespace NMib::NWeb;
+#endif
