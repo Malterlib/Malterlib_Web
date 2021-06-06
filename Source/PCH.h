27cb88a5561fddd57dfca4ef29293f4e620bb760
Erik Olofsson <erik@olofsson.info>
2021-06-07 00:03:27 +0200

    [Malterlib/Web] Unsplit prefix headers on Xcode

--- Source/PCH.h
+++ Source/PCH.h
@@ -1,6 +1,10 @@
 ﻿// Copyright © 2015 Hansoft AB 
 // Distributed under the MIT license, see license text in LICENSE.Malterlib
 
+#ifdef __cplusplus
 #include <Mib/Core/Core>
 #include <Mib/Concurrency/ConcurrencyManager>
 
+#else
+#include <stdlib.h>
+#endif
