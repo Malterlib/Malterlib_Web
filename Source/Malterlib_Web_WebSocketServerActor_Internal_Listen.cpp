323c396ccb2b5bb6d573733c4daff563a0d6656c
Erik Olofsson <erik@olofsson.info>
2026-09-09 20:24:09 +0200

    Use cancellable address lists in WebSocket clients and listeners
    Keep lookup cancellation subscriptions alive while awaiting address vectors. Adapt connection and listener setup to the resolver API, including builds where warning logs are disabled.

--- Source/Malterlib_Web_WebSocketServerActor_Internal_Listen.cpp
+++ Source/Malterlib_Web_WebSocketServerActor_Internal_Listen.cpp
@@ -121,7 +121,7 @@ namespace NMib::NWeb::NWebSocket
 						;
 					}
 				}
-				catch (NException::CException const &_Exception)
+				catch ([[maybe_unused]] NException::CException const &_Exception)
 				{
 					DMibLogWithCategory(Mib/Web, Warning, "Accepting a websocket connection failed: {}", _Exception);
 				}
